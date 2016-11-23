84e9215bdc5fda1b9e9afe97bbd82d85cd6642d3
yujoi <joooi.y1313@gmail.com>
2016-11-23 20:31:39 +0900

    Test

--- app/src/main/java/com/example/yujoi/myapplication/CustomTabActivity.java
+++ app/src/main/java/com/example/yujoi/myapplication/CustomTabActivity.java
@@ -1,9 +1,36 @@
 package com.example.yujoi.myapplication;
 
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by yujoi on 2016/11/06.
  */
-public class CustomTabActivity{
+public class CustomTabActivity extends AppCompatActivity {
+
+    private String TAG = "CustomTabs";
+
+    private CustomTabsSession mCustomTabsSession;
+    private CustomTabsClient mClient;
+    private CustomTabsServiceConnection mConnection;
+    private String mPackageNameToBind;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
