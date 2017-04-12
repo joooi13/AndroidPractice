87dfec12e611d9673b33ed2250d94400d97aac82
yujoi <joooi13@users.noreply.github.com>
2017-04-12 23:20:33 +0900

    commit test

--- app/src/main/java/com/example/joy/myapplication/ImageSampleActivity.java
+++ app/src/main/java/com/example/joy/myapplication/ImageSampleActivity.java
@@ -19,6 +19,7 @@ public class ImageSampleActivity extends Activity{
         setContentView(R.layout.image_sample);
 
         //Glide
+        //commit???
         ImageView imageView = (ImageView) findViewById(R.id.img);
         Glide.with(this).load("https://goo.gl/rjX8kD").into(imageView);
     }
